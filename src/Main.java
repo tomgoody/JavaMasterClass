10ed4e3bb5fe237f2a328036cf113975741dcbf1
tgoody <tgoody1986>
2021-04-25 12:51:31 -0600

    initial push

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,62 @@
 
 public class Main {
     public static void main(String[] args) {
+/*       int newScore = calculateScore("Tom", 500);
+        System.out.println("New score is " + newScore);
 
-        MoreMethodPractice.smallerNumber(25,37,29);
-        MoreMethodPractice.smallerNumber(30,19,20);
-        MoreMethodPractice.smallerNumber(20,32,10);
-        System.out.println();
-        MoreMethodPractice.average(25, 45, 65);
-        MoreMethodPractice.average(1, 2, 3);
-
-        MoreMethodPractice.compoundInterest(1000, 10, 5);
-
+        calculateScore(75);
+        calculateScore();
 
     }
+
+    public static int calculateScore(String playerName, int score){
+        System.out.println("Player " + playerName + " scored " + score + " points.");
+        return score * 1000;
+    }
+
+    public static int calculateScore( int score){
+        System.out.println("Unnamed player scored " + score + " points" );
+        return score * 1000;
+    }
+
+    public static int calculateScore(){
+        System.out.println("No player name, no player score." );
+        return 0;
+        System.out.println(areaCalculator.area(3));
+        System.out.println(areaCalculator.area(2, 4));
+
+                    String stringYears = years + " y and ";
+            if (years < 10 && years > 0) {
+                stringYears = "0" + stringYears;
+            }
+
+            String stringDays = remainingDays + " d";
+            if (remainingDays < 10 && remainingDays > 0) {
+                stringDays = "0" + stringDays;
+            }
+
+
+
+
+
+        MinutesToYearsDaysCalculator.printYearsAndDays(532800);
+        MinutesToYearsDaysCalculator.printYearsAndDays(5270400);
+        MinutesToYearsDaysCalculator.printYearsAndDays(0);
+
+ */
+
+        IntEqualityPrinter.printEqual(1,1,1);
+        IntEqualityPrinter.printEqual(1,1,2);
+        IntEqualityPrinter.printEqual(-1,-1,-1);
+        IntEqualityPrinter.printEqual(1,2,3);
+
+        int a, b,c;
+        a = 1;
+        b = 2;
+        c = 3;
+        int d = (a % b);
+        int e = (a % c);
+        System.out.println(d + " and "+ e);
+    }
 }
+
---
+++ src/SecondsAndMinutesChallenge.java
@@ -0,0 +1,34 @@
+public class SecondsAndMinutesChallenge {
+
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
+
+
+    public static void main(String[] args) {
+       System.out.println(getDurationString(65, -1));
+       System.out.println(getDurationString(10));
+    }
+
+    public static String getDurationString(long minutes, long seconds){
+        if(minutes < 0 || (seconds < 0)){
+            return INVALID_VALUE_MESSAGE;
+
+        } if (seconds > 59){
+            minutes += (seconds / 60);
+            seconds = (seconds % 60);
+        }
+        minutes += (seconds / 60);
+        seconds = (seconds % 60);
+        long hours = (minutes / 60);
+        minutes = (minutes % 60);
+        return hours + "h " + minutes + "m " + seconds + "s ";
+    }
+
+    public static String getDurationString(long seconds){
+        if (seconds < 0){
+            return INVALID_VALUE_MESSAGE;
+        }
+        long minutes = (seconds / 60);
+        long remainingSeconds = (seconds % 60);
+        return getDurationString(minutes, remainingSeconds);
+    }
+}
